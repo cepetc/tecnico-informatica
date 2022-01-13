62608c12e749de2b2cb3505d2a96f7615fd073fc
phmiranda <pehhagah.1607@gmail.com>
2022-01-12 21:49:33 -0300

    Instalação de dependências e configuração do projeto.
    Instalação de dependências e configuração do projeto.

---
+++ src/controller/UsuarioController.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Author: phmiranda
+ * Project: tecnico-basico
+ * Task Number: HUXXX
+ * Description: N/A
+ * Date: 12/01/2022
+ */
+
+namespace Pedro\TecnicoBasico;
+
+class UsuarioController
+{
+
+}
---
+++ src/domain/Usuario.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Author: phmiranda
+ * Project: tecnico-basico
+ * Task Number: HUXXX
+ * Description: N/A
+ * Date: 12/01/2022
+ */
+
+namespace Pedro\TecnicoBasico;
+
+class Usuario {
+
+}
